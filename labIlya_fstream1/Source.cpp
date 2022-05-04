365b8b5eb3649ffbafe3bc7d452edb843238475a
illy skalikxyb <pagany10@gmail.com>
2022-05-04 22:05:46 +0200

    commit 1

--- labIlya_fstream1/Source.cpp
+++ labIlya_fstream1/Source.cpp
@@ -7,6 +7,28 @@ using namespace std;
 
 int main()
 {
+	ifstream file;
+	string word, strfile;
+	int countr = 0;
+	file.open("file.txt", ios::in);
+	if (!file.is_open())
+	{
+		cout << "Invalid!";
+	}
+	else
+	{
+		cout << "Enter word: ";
+		cin >> word;
+		while (getline(file, strfile))
+		{
+			char* pch = strtok((char*)strfile.c_str(), ".!?");
+			while (pch != NULL)
+			{
 
+			}
+		}
+		cout << "The number of the word " << word << ": " << countr << endl;
+	}
+	file.close();
 	return 0;
 }
