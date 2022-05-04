35128bfc29e16281954adac2e3080ad8b8299850
illy skalikxyb <pagany10@gmail.com>
2022-05-04 11:57:53 +0200

    Done exercise #2

--- labIlya_fstream1/Source.cpp
+++ labIlya_fstream1/Source.cpp
@@ -7,6 +7,27 @@ using namespace std;
 
 int main()
 {
-
+	ifstream file;
+	string word, strfile;
+	int countr = 0;
+	file.open("file.txt", ios::in);
+	if (!file.is_open())
+	{
+		cout << "Invalid!";
+	}
+	else
+	{
+		while (getline(file, strfile))
+		{
+			char* pch = strtok((char*)strfile.c_str(), ".!?");
+			while (pch != NULL)
+			{
+				countr++;
+				pch = strtok(NULL, ".!?");
+			}
+		}
+		cout << "The number of sentences: " << countr << endl;
+		file.close();
+	}
 	return 0;
 }
