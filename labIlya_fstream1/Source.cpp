d74899acb82a06d3f377af91d4cb33ecde51f779
illy skalikxyb <pagany10@gmail.com>
2022-05-04 15:30:59 +0200

    Done exercise #3

--- labIlya_fstream1/Source.cpp
+++ labIlya_fstream1/Source.cpp
@@ -7,6 +7,33 @@ using namespace std;
 
 int main()
 {
-
+	ifstream file;
+	string word, strfile;
+	int dots = 0, commas = 0;
+	file.open("file.txt", ios::in);
+	if (!file.is_open())
+	{
+		cout << "Invalid!";
+	}
+	else
+	{
+		while (getline(file, strfile))
+		{
+			for (int i = 0; i < strfile.size();i++)
+			{
+				if (strfile[i] == ',')
+				{
+					commas++;
+				}
+				if (strfile[i] == '.')
+				{
+					dots++;
+				}
+			}
+		}
+		cout << "The number of dots: " << dots << endl;
+		cout << "The number of commas: " << commas << endl;
+		file.close();
+	}
 	return 0;
 }
