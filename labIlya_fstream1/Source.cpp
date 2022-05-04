ebd067ad0a0e8015b8e8d8078709e53c478000d6
illy skalikxyb <pagany10@gmail.com>
2022-05-04 22:23:26 +0200

    commit 2

--- labIlya_fstream1/Source.cpp
+++ labIlya_fstream1/Source.cpp
@@ -8,27 +8,39 @@ using namespace std;
 int main()
 {
 	ifstream file;
-	string word, strfile;
-	int countr = 0;
+	ofstream file2;
+
+	string longstr, strfile;
+	int len = 0, n = 0;
 	file.open("file.txt", ios::in);
-	if (!file.is_open())
+	file2.open("file2.txt", ios::out);
+	if (!file.is_open() && !file2.is_open())
 	{
 		cout << "Invalid!";
 	}
 	else
 	{
-		cout << "Enter word: ";
-		cin >> word;
 		while (getline(file, strfile))
 		{
 			char* pch = strtok((char*)strfile.c_str(), ".!?");
 			while (pch != NULL)
 			{
-
+				longstr += strfile;
+				pch = strtok(NULL, ".!?");
 			}
+			cout << longstr << endl;
+			len = longstr.length();
+			n = len - 1;
+			for (int i = 0; i < (len / 2); i++)
+			{
+				swap(longstr[i], longstr[n]);
+				n = n - 1;
+			}
+			file2 << longstr;
+			cout << longstr << endl;
 		}
-		cout << "The number of the word " << word << ": " << countr << endl;
 	}
 	file.close();
+	file2.close();
 	return 0;
 }
