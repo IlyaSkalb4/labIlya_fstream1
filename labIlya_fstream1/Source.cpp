35f9de5918b20e1c31617cdb4fec8afdc9bacefb
illy skalikxyb <pagany10@gmail.com>
2022-05-04 19:11:31 +0200

    Done exercise #4

--- labIlya_fstream1/Source.cpp
+++ labIlya_fstream1/Source.cpp
@@ -7,6 +7,35 @@ using namespace std;
 
 int main()
 {
-
+	ifstream file;
+	ofstream file2;
+	string strfile, longstr;
+	int len = 0, n = 0;
+	file.open("file.txt", ios::in);
+	file2.open("file2.txt", ios::out);
+	if (!file.is_open() && !file2.is_open())
+	{
+		cout << "Invalid!";
+	}
+	else
+	{
+		while (getline(file, strfile))
+		{
+			longstr += strfile;
+			longstr += "\n";
+		}
+		cout << longstr << endl;
+		len = longstr.length();
+		n = len - 2;
+		for (int i = 0; i < (len / 2); i++)
+		{
+			swap(longstr[i], longstr[n]);
+			n = n - 1;
+		}
+		file2 << longstr;
+		cout << longstr << endl;
+	}
+	file.close();
+	file2.close();
 	return 0;
 }
