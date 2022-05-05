f21bd0c0ec203471664f53021cac0271fb14fbd8
illy skalikxyb <pagany10@gmail.com>
2022-05-05 15:58:34 +0200

    Done exercise #5

--- labIlya_fstream1/Source.cpp
+++ labIlya_fstream1/Source.cpp
@@ -5,13 +5,23 @@
 
 using namespace std;
 
+void reverse_String(string& greet, int n, int i) {
+
+	if (n <= i) 
+	{
+		return; 
+	}
+	swap(greet[i], greet[n]);
+	reverse_String(greet, n - 1, i + 1);
+}
+
 int main()
 {
 	ifstream file;
 	ofstream file2;
 
 	string longstr, strfile;
-	int len = 0, n = 0;
+	int len = 0, n = 0, i = 1, k = 1, countr1 = 0, num = 0, maximum = -99999, allsentences = 0;
 	file.open("file.txt", ios::in);
 	file2.open("file2.txt", ios::out);
 	if (!file.is_open() && !file2.is_open())
@@ -22,23 +32,30 @@ int main()
 	{
 		while (getline(file, strfile))
 		{
-			char* pch = strtok((char*)strfile.c_str(), ".!?");
+			char* pch = strtok((char*)strfile.c_str(), ".");
 			while (pch != NULL)
 			{
-				longstr += strfile;
-				pch = strtok(NULL, ".!?");
+				longstr = pch;
+				len = longstr.length();
+				n = len - 1;
+				reverse_String(longstr, n, i);
+				file2 << longstr << ".";
+				pch = strtok(NULL, ".");
+				countr1++;
+				allsentences++;
 			}
-			cout << longstr << endl;
-			len = longstr.length();
-			n = len - 1;
-			for (int i = 0; i < (len / 2); i++)
+			cout << "Sentensec in string #" << k << ": " << countr1 << endl;
+			file2 <<"\n";
+			if (maximum < countr1)
 			{
-				swap(longstr[i], longstr[n]);
-				n = n - 1;
+				maximum = countr1;
+				num = k;
 			}
-			file2 << longstr;
-			cout << longstr << endl;
+			countr1 = 0;
+			k++;
 		}
+		cout << "Total reverse sentences: " << allsentences << endl;
+		cout << "Maximum sentences in string #" << num << endl;
 	}
 	file.close();
 	file2.close();
